16906440312f7cdfb45232b919789b1657fd68e7
saininda-Zayr <saininda@gmail.com>
2026-04-07 00:25:40 +0800

    Set base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/Portal-PIK-P-Digital-Hub/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
